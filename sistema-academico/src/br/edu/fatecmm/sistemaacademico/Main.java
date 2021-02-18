20d45ccb0b9bc26e97dc82501344b4f47a5f4ae9
maromo71m <professormoraes@gmail.com>
2021-02-18 09:07:08 -0300

    Aula do dia 18 de fev

---
+++ sistema-academico/src/br/edu/fatecmm/sistemaacademico/Main.java
@@ -0,0 +1,12 @@
+package br.edu.fatecmm.sistemaacademico;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 9;
+        int b = 10;
+        int soma = a + b;
+        System.out.printf("Valor da soma: %d \n", soma);
+    }
+}
